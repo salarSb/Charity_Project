b95d65a346642b9b438dba78c01671bdbfeb0bc2
salar <salarsabeti88@gmail.com>
2021-08-23 12:51:49 +0430

    add python decouple package to secure the project

--- back_end/charity/settings.py
+++ back_end/charity/settings.py
@@ -1,13 +1,13 @@
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd8+lpdyupy-+snp_@iwh3bz)a0@2xf&==4x$v&bqw*axn@2c$d'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
